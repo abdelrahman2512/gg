5464ea446c60045f2c5875071f26e843ee1c42bd
abdelrahman2512 <105170957+abdelrahman2512@users.noreply.github.com>
2023-03-23 21:35:28 +0200

    Update users_sql.py

--- StringSessionBot/database/users_sql.py
+++ StringSessionBot/database/users_sql.py
@@ -15,7 +15,7 @@ class Users(BASE):
     #     return "<User {} {} {} ({})>".format(self.thumbnail, self.thumbnail_status, self.video_to, self.user_id)
 
 
-Users.__table__.create(checkfirst=True,bind="users")
+Users.__table__.create()
 
 
 async def num_users():
